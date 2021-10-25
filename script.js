a59ecc021a5d1511fc52344c68b2e8f2fb628e7d
pereus <69439057+pereus@users.noreply.github.com>
2021-10-25 17:14:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 (() => {
 const inputEl = document.getElementById("input-field");
 const buttonEl = document.getElementById("btn");
+const listEl = document.getElementById("list");
+const templateEl = document.getElementById('template').innerHTML;
+const onDeleteEls = evt => {
+    evt.target.parentElement.remove()
+}
+const onThroughEl = evt => {
+    evt.target.nextElementSibling.classList.add('strikethrough-text')
+}
 
 buttonEl.addEventListener('click', () => {
- console.log(inputEl.value);
+    const contentEl = inputEl.value;
+    if (!contentEl) {
+        return;
+    }
+    const listItemEl = document.createElement('li');
+    listItemEl.innerHTML = templateEl.replace('{{contentEl}}', contentEl);
     inputEl.value = '';
+
+    listEl.append(listItemEl);
+    listItemEl.querySelector('#btn-delete').addEventListener('click', onDeleteEls);
+    listItemEl.querySelector('.checkbox').addEventListener('change', onThroughEl)
+    
 })
 }
-)();
+)()
