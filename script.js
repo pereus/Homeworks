e1d8073d338b861160d87cdaf07a70a7b2c92cda
pereus <69439057+pereus@users.noreply.github.com>
2021-10-25 20:22:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-
+function recursion(n) {
+    if ((n <= 1) && (n > -1)) {
+        return n;
+    } else if (n < 0) {
+        return recursion(n+2) - recursion(n+1);
+    } else {
+        return recursion(n-1) + recursion(n-2);
+    };
+};
 do {
     let fib;
     do {
         fib = +prompt('Enter the number to calculate Fibonacci');
-    } while (fib !== fib);
-
-    function recursion(n) {
-        if ((n <= 1) && (n > -1)) {
-            return n;
-        } else if (n < 0) {
-            return recursion(n+2) - recursion(n+1);
-        } else {
-            return recursion(n-1) + recursion(n-2);
-        };
-    };
-    
+        } while (fib !== fib);
     console.log(recursion(fib));
     alert(`Result of calculation Fibonacci is ${recursion(fib)}`);
 } while (confirm('Do you want to repeat?'));
-
